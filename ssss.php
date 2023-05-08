3b79f2d23261eab9eb03d8e388847d029ca13392
mo-peligroso <131658582+mo-peligroso@users.noreply.github.com>
2023-05-08 11:23:16 +0200

    first commit

---
+++ ssss.php
@@ -0,0 +1,23 @@
+<?php
+$var1 = 12;
+$var2 = 10;
+$eind = $var1 + $var2;
+
+echo "Het antwoord is " . $eind;
+
+$var3 = 12;
+$var4 = 10;
+$eind2 = $var3 * $var4;
+
+echo "Het antwoord is " . $eind2;
+
+$naam = "Mohamed";
+echo "Welkom op de pagina van " . $naam;
+
+/* deze code bevat veel fouten
+haal alle fouten eruit!
+*/
+
+$mijnnaam = "Carl";
+$jouwnaam = "Camel";
+echo $mijnnaam . " zit bij " . $jouwnaam . " in de klas!";
